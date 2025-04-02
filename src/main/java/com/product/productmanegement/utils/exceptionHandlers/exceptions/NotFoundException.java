d11a9dc67a7bb55eb4fb1e0f797cb0d775867bfc
Rishabh Mishra <rishabh.mishra@ofbusiness.in>
2025-04-02 15:53:53 +0530

    initial commit

--- src/main/java/com/product/productmanegement/utils/exceptionHandlers/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.product.productmanegement.utils.exceptionHandlers.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
